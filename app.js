e74af3ac72087997be720d96eba79afb837704bd
fazliddinofficial <fazliddinquvatboyev8@gmail.com>
2024-08-21 04:19:37 -0700

    google form app has been created

---
+++ app.js
@@ -0,0 +1,13 @@
+function ulDownOne() {
+    let mainBoxUl = document.getElementById('box-ul');
+    mainBoxUl.style.display = 'flex';
+}
+function ulDownTwo() {
+    let mainBoxUl = document.getElementById('box-ul-two');
+    mainBoxUl.style.display = 'flex';
+}
+let mySet = new Set();
+function forMain() {
+    let main = document.getElementById('main');
+    
+}
